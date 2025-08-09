af2343d6bcbe9865455bdad9353a23e819de5cf5
bynmch <byunmch@naver.com>
2025-08-09 11:37:41 +0900

    커피심부름

---
+++ mincheol/Week02/src/main/java/LV0/problem1/CoffeeErrand.java
@@ -0,0 +1,8 @@
+package LV0.problem1;
+
+public class CoffeeErrand {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(new String[] {"cafelatte", "americanoice", "hotcafelatte", "anything"}));
+        System.out.println(Solution.solution(new String[] {"americanoice", "americano", "iceamericano"}));
+    }
+}
---
+++ mincheol/Week02/src/main/java/LV0/problem1/Solution.java
@@ -0,0 +1,28 @@
+package LV0.problem1;
+
+/*
+ * 이름: 변민철
+ * 날짜: 25. 8. 9.
+ * 문제: https://school.programmers.co.kr/learn/courses/30/lessons/181837
+ * */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static int solution(String[] order) {
+        int answer = 0;
+        String[] Latte = new String[] {"cafelatte", "icecafelatte","cafelatteice", "hotcafelatte", "cafelattehot"};
+        List<String> LatteList = new ArrayList<>(Arrays.asList(Latte));
+
+        for(int i = 0; i < order.length; i++){
+            if (LatteList.contains(order[i])) {
+                answer += 5000;
+            } else {
+                answer += 4500;
+                }
+        }
+        return answer;
+    }
+}
