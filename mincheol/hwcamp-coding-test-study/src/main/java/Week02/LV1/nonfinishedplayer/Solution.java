f9b47d1e72dd023c6bf23b8165e8bc9b3316feb9
bynmch <byunmch@naver.com>
2025-08-13 14:01:45 +0900

    완주하지 못한 선수

--- mincheol/hwcamp-coding-test-study/src/main/java/Week02/LV1/nonfinishedplayer/Solution.java
+++ mincheol/hwcamp-coding-test-study/src/main/java/Week02/LV1/nonfinishedplayer/Solution.java
@@ -6,12 +6,30 @@ package Week02.LV1.nonfinishedplayer;
  * 문제: https://school.programmers.co.kr/learn/courses/30/lessons/42576
  * */
 
+import java.util.*;
+
 public class Solution {
     public static String solution(String[] participant, String[] completion) {
         String answer = "";
-        주어진 완주자 문자열 배열의 값들을 해시 테이블에 담는다.
-                주어진 참가자 문자열 배열 값들을 하나씩 꺼내어 값이 완주자 문자열 배열에 있는지 () -> 확인한다
-                 없다면 해당 값을 출력한다.
+
+        Map<String, Integer> pHashMap = new HashMap<>();
+
+        // Map 내에 참가자 목록(participant)을 추가 참가자 이름(key), 완주여부 체크(value) - 0: 완주, 1: 미완주
+        for (String participantItem : participant) {
+            pHashMap.put(participantItem, pHashMap.getOrDefault(participantItem, 0) + 1);
+        }
+
+        // 완주 한 인원을 체크 Key 값에 따라 업데이트 수행, 완주여부에 따라 완주여부 체크(value)를 -1을 해서 완주여부확인을 수행
+        for (String completionItem : completion) {
+            pHashMap.put(completionItem, pHashMap.get(completionItem) - 1);
+        }
+
+        // Map을 순회하면서 0이 아닌 값을 찾아서 결과로 반환
+        for (Map.Entry<String, Integer> entry : pHashMap.entrySet()) {
+            if (entry.getValue() != 0) {
+                answer = entry.getKey();
+            }
+        }
         return answer;
     }
 }
