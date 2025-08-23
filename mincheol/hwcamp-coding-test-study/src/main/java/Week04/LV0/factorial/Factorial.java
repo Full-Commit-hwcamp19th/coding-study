cb9a97b20da33741f23ebcb3f7ff93d7397b00fd
bynmch <byunmch@naver.com>
2025-08-23 17:29:47 +0900

    4주차 LV0 팩토리얼

---
+++ mincheol/hwcamp-coding-test-study/src/main/java/Week04/LV0/factorial/Factorial.java
@@ -0,0 +1,9 @@
+package Week04.LV0.factorial;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(3628800));
+        System.out.println(Solution.solution(7));
+    }
+}
