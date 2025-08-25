878b5d02b55ffafc49410ebf6c3626ad10a6e009
bynmch <byunmch@naver.com>
2025-08-25 19:34:54 +0900

    3주차 LV2 의상

---
+++ mincheol/hwcamp-coding-test-study/src/main/java/Week03/LV2/clothes/Clothes.java
@@ -0,0 +1,10 @@
+package Week03.LV2.clothes;
+
+public class Clothes {
+
+    public static void main(String[] args) {
+
+        System.out.println(Solution.solution(new String[][] {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}}));
+        System.out.println(Solution.solution(new String[][] {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}}));
+    }
+}
