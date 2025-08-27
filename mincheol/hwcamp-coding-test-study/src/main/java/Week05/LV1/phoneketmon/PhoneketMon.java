f7507e71856dbde979ed3e9f67ba76a4ffa361a6
bynmch <byunmch@naver.com>
2025-08-27 14:04:05 +0900

    5주차 LV1 폰켓몬

---
+++ mincheol/hwcamp-coding-test-study/src/main/java/Week05/LV1/phoneketmon/PhoneketMon.java
@@ -0,0 +1,9 @@
+package Week05.LV1.phoneketmon;
+
+public class PhoneketMon {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(new int[]{3, 1, 2, 3}));
+        System.out.println(Solution.solution(new int[]{3, 3, 3, 2, 2, 4}));
+        System.out.println(Solution.solution(new int[]{3, 3, 3, 2, 2, 2}));
+    }
+}
