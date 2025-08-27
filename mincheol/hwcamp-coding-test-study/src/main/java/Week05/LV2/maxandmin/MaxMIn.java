2f41fc22ade605cd5e80fcb71082236435b9d6fb
bynmch <byunmch@naver.com>
2025-08-27 16:13:10 +0900

    5주차 LV2 최댓값과 최솟값

---
+++ mincheol/hwcamp-coding-test-study/src/main/java/Week05/LV2/maxandmin/MaxMIn.java
@@ -0,0 +1,9 @@
+package Week05.LV2.maxandmin;
+
+public class MaxMIn {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution("1 2 3 4"));
+        System.out.println(Solution.solution("-1 -2 -3 -4"));
+        System.out.println(Solution.solution("-1 -1"));
+    }
+}
