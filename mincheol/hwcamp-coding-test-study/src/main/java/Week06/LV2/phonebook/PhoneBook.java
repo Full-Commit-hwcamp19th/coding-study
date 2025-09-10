132996ea467e6684c88009db418ee58112c05e82
bynmch <byunmch@naver.com>
2025-09-10 11:59:05 +0900

    6주차 LV2 전화번호 목록

---
+++ mincheol/hwcamp-coding-test-study/src/main/java/Week06/LV2/phonebook/PhoneBook.java
@@ -0,0 +1,9 @@
+package Week06.LV2.phonebook;
+
+public class PhoneBook {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(new String[]{"119", "97674223", "1195524421"}));
+        System.out.println(Solution.solution(new String[]{"123","456","789"}));
+        System.out.println(Solution.solution(new String[]{"12","123","1235","567","88"}));
+    }
+}
